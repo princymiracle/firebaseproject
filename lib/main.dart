1a9f7a24430d1f043a78d2f27b1d3c270eaec201
princymiracle <princy.miracle@gmail.com>
2024-02-22 16:35:03 +0530

    init commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,82 @@
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_messaging/firebase_messaging.dart';
+// import 'package:firebaseproject/Authentication/Login.dart';
+// import 'package:firebaseproject/Authentication/loginwithgoogle/loginwithgoogle.dart';
+// import 'package:firebaseproject/Notifications/Home_Screen.dart';
+// import 'package:firebaseproject/Notifications/Notification_Servies.dart';
+// import 'package:firebaseproject/firebase_options.dart';
+// import 'package:firebaseproject/ui/splashscreen.dart';
+// import 'package:flutter/material.dart';
+// import 'package:get/get_navigation/src/root/get_material_app.dart';
+//
+//
+// @pragma('vm:entry-point')
+// Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+//   // If you're going to use other Firebase services in the background, such as Firestore,
+//   // make sure you call `initializeApp` before using other Firebase services.
+//   await Firebase.initializeApp();
+//
+//   print("Handling a background message: ${message.messageId}");
+// }
+//
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp(
+//       options: DefaultFirebaseOptions.currentPlatform
+//   );
+//   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+//   runApp(const MyApp());
+// }
+//
+//
+// // getToken() async{
+// //   String? token = await MyNotification.messaging.getToken();
+// //   print("token ${token}");
+// // }
+//
+// class MyApp extends StatefulWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
+//
+// class _MyAppState extends State<MyApp> {
+//
+//   void initState() {
+//     MyNotification.requestPermission();
+//     MyNotification.listenMessage();
+//     getToken();
+//     super.initState();
+//   }
+//   getToken() async{
+//     String? token = await MyNotification.messaging.getToken();
+//     print("token ${token}");
+//   }
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return GetMaterialApp(
+//       title: 'Flutter Demo',
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData(
+//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//         useMaterial3: true,
+//       ),
+//       home: PhoneHome(),
+//     );
+//   }
+// }
+//
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:firebaseproject/Notifications/Home_Screen.dart';
+import 'package:firebaseproject/Authentication/Login.dart';
+import 'package:firebaseproject/Authentication/loginwithgoogle/loginwithgoogle.dart';
 import 'package:firebaseproject/Notifications/Notification_Servies.dart';
 import 'package:firebaseproject/firebase_options.dart';
-import 'package:firebaseproject/ui/splashscreen.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 
 @pragma('vm:entry-point')
@@ -16,7 +88,7 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   print("Handling a background message: ${message.messageId}");
 }
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform
@@ -25,21 +97,14 @@ void main() async {
   runApp(const MyApp());
 }
 
-//
-// getToken() async{
-//   String? token = await MyNotification.messaging.getToken();
-//   print("token ${token}");
-//
-// }
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
-
 class _MyAppState extends State<MyApp> {
-
+  @override
   void initState() {
     MyNotification.requestPermission();
     MyNotification.listenMessage();
@@ -49,18 +114,21 @@ class _MyAppState extends State<MyApp> {
   getToken() async{
     String? token = await MyNotification.messaging.getToken();
     print("token ${token}");
+
   }
+
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: SplashScreen(),
+      home:  PhoneHome(),
     );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,19 +6,25 @@ import FlutterMacOS
 import Foundation
 
 import cloud_firestore
+import cloud_functions
 import firebase_auth
 import firebase_core
 import firebase_messaging
 import firebase_storage
 import flutter_local_notifications
+import google_sign_in_ios
+import smart_auth
 import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  FLTFirebaseFunctionsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFunctionsPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+  SmartAuthPlugin.register(with: registry.registrar(forPlugin: "SmartAuthPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
