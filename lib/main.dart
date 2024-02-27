fe1bd4d299efcc2ead9bf69252fa73bf2b9344b3
princymiracle <princy.miracle@gmail.com>
2024-02-27 14:38:52 +0530

    init commit

--- lib/main.dart
+++ lib/main.dart
@@ -128,7 +128,7 @@ class _MyAppState extends State<MyApp> {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:  GlassMorphism(),
+      home:  PhoneHome(),
     );
   }
 }
